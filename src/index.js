8431724dbdcb3715d3ef8223c5ce08989dfa4bb6
Vitor <vitorzaffari01@gmail.com>
2023-05-04 15:50:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+const Store = require('electron-store')
+const store = new Store()
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -13,6 +16,8 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation:false
     },
   });
 
